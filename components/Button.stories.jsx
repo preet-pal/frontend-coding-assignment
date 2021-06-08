4b0f07d8f2343950723f0703c8e9fad6c2211299
Bastiaan van Graafeiland <bastiaan89@live.nl>
2021-06-08 16:14:20 +0200

    setup storybook

---
+++ components/Button.stories.jsx
@@ -0,0 +1,6 @@
+import Button from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+}
---
+++ components/Button.tsx
@@ -0,0 +1,5 @@
+export default function Button() {
+    return (
+      <button className="bg-blue-500 text-white">Click me</button>
+    );
+}
